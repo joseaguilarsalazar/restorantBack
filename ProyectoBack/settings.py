7727cf17e8b696323926b604e261905121eb1b4f
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-08 21:07:38 -0500

    first commit ab

--- ProyectoBack/settings.py
+++ ProyectoBack/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'ProyectoBack.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'mydatabase'),
-        'USER': os.environ.get('DB_USER', 'myuser'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME', 'your_db_name'),
+        'USER': os.getenv('DB_USER', 'your_db_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_db_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # <-- Change this!
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
