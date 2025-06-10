a1259e6d22289efeafb147efecbb29d15203b3ab
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-09 19:54:44 -0500

    auto super user added

--- ProyectoBack/urls.py
+++ ProyectoBack/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 # ✅ Swagger schema configuration
 schema_view = get_schema_view(
@@ -42,4 +45,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
