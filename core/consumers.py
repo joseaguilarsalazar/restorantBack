2a1aa7afdf2d4985e3c9214662e7d602fc5950c3
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-24 21:13:06 -0500

    reddis configured

--- core/consumers.py
+++ core/consumers.py
@@ -2,42 +2,42 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
 
+from .models import Pedido
+
 
 class PedidoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Join group
         await self.channel_layer.group_add("realtime_updates", self.channel_name)
         await self.accept()
-        # 📤 Send initial data on connect
         await self.send_initial_data()
         print("WebSocket connected:", self.channel_name)
 
     async def disconnect(self, close_code):
-        # Leave group
         await self.channel_layer.group_discard("realtime_updates", self.channel_name)
         print("WebSocket disconnected:", self.channel_name)
 
-    # This method name matches the "type" value from group_send
     async def send_data(self, event):
-        # event["data"] is the list of pedidos we sent from the signal
         await self.send(text_data=json.dumps({
             "type": "pedido_update",
             "payload": event["data"]
         }))
 
     async def send_initial_data(self):
-        from .models import Pedido
-        pedidos = await sync_to_async(list)(Pedido.objects.exclude(estado="pagado"))
-        data = [{
-            'id': p.id,
-            'mesa': p.mesa.id if p.mesa else None,
-            'plato': p.plato.name if p.plato else None,
-            'cantidad': p.cantidad,
-            'datetime': p.fecha.isoformat(),
-            'estado': p.estado
-        } for p in pedidos]
-
+        data = await get_pedidos_data()
         await self.send(text_data=json.dumps({
             "type": "send_data",
             "data": data
         }))
+
+
+@sync_to_async
+def get_pedidos_data():
+    pedidos = Pedido.objects.select_related('mesa', 'plato').exclude(estado="pagado")
+    return [{
+        'id': p.id,
+        'mesa': p.mesa.id if p.mesa else None,
+        'plato': p.plato.name if p.plato else None,
+        'cantidad': p.cantidad,
+        'datetime': p.fecha.isoformat(),
+        'estado': p.estado
+    } for p in pedidos]
